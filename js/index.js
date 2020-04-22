6f175a7b4d5abd49ae279e95eb829ac2e863cebd
Patryk <patrykyrtap@o2.pl>
2020-04-22 23:01:59 +0200

    Initialize repository
    Create some basics of the game,
    create directory and files,
    simple styles

---
+++ js/index.js
@@ -0,0 +1,51 @@
+//Create map with blocks
+for(let i=0; i<72; i++){
+        const block = document.createElement('div');
+        const map = document.getElementById('map');
+        block.className = 'square emptyPlace';
+        block.dataset.modalTarget = "#emptyPlace";
+        block.id = `b${i}`;
+        map.appendChild(block);
+}
+
+//Town hall
+const townHall = document.getElementById('b30');
+townHall.style.backgroundImage = "url('./assets/images/townHall/townHall-lvl1.png')";
+townHall.style.backgroundSize = "cover";
+townHall.className = "square building";
+townHall.dataset.modalTarget = "#townHall";
+
+
+
+
+//Sawmill
+const sawmill = document.getElementById('b15');
+sawmill.style.backgroundImage = "url('./assets/images/sawmill/sawmill-lvl1.png')";
+sawmill.style.backgroundSize = "cover";
+sawmill.className = "square building";
+sawmill.dataset.modalTarget = "#sawmill";
+
+
+
+//Quarry
+//Academy
+//Barrack
+//Armory
+//Market
+//UnderConstruction
+//Road
+
+
+const click = document
+
+
+//Empty Place
+//const emptyPlace = document.dataset.modalTarget = "#emptyPlace";
+// if (document.dataset.modalTarget == "#emptyPlace"){
+//     const emptyPlace = document.getElementById('#emptyPlace')
+// }
+// {
+//     if(document.dataset.modalTarget == "#emptyPlace"){
+//         console.log("mom0");
+//     }
+// }
---
+++ js/popUp.js
@@ -0,0 +1,45 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.dataset.modalTarget !== "#emptyPlace"){
+            const modal = document.querySelector(button.dataset.modalTarget);
+            openModal(modal);
+            console.log(button.id);
+            console.log(button.dataset.modalTarget);
+        }else{
+            const modal = document.querySelector("#emptyPlace");
+            console.log(button.id);
+            openModal(modal);
+        }  
+        // const modal = document.querySelector(button.dataset.modalTarget);
+        // openModal(modal);
+    })
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+});
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
